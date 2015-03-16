d9e815851a1f6e1bc8353d1732adf2a9348c3ace
Adrien Thery <adrien.thery@gmail.com>
2015-03-16 20:15:05 +0100

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,10 @@ adrien  = Risebox::Core::User.create first_name: 'Adrien', last_name: 'Thery', e
 nicolas = Risebox::Core::User.create first_name: 'Nicolas', last_name: 'Nardone', email: 'nicolas@risebox.co'
 
 menucourt = adrien.devices.create(name: 'menucourt', key: 'menucourt', model: 'Hapy', version: '1')
-lab1      = nicolas.devices.create(name: 'lab1', key: 'lab1', model: 'Hapy', version: '2')
+lab1      = nicolas.devices.create(name: 'Electrolab One', key: 'lab1', model: 'Hapy', version: '2')
+
+menucourt.update_attributes token: 'token1'
+lab1.update_attributes token: 'token2'
 
 ph        = Risebox::Core::Metric.create name: "PH de l'eau",                   code: 'PH',    unit: nil,       display_order: 1
 w_temp    = Risebox::Core::Metric.create name: "Température de l'eau",          code: 'WTEMP', unit: '°C',      display_order: 2
@@ -14,12 +17,14 @@ no2       = Risebox::Core::Metric.create name: "Taux de nitrites (NO2)",        
 nh4       = Risebox::Core::Metric.create name: "Taux d'ammonium (NH4)",         code: 'NH4',   unit: 'mg/l',    display_order: 8
 no3       = Risebox::Core::Metric.create name: "Taux de nitrates (NO3)",        code: 'NO3',   unit: 'mg/l',    display_order: 9
 
-lab1.metric_statuses.create(metric: ph,      limit_min: 6.5, limit_max: 7.8)
-lab1.metric_statuses.create(metric: w_temp,  limit_min: 18,  limit_max: 25)
-lab1.metric_statuses.create(metric: a_temp,  limit_min: 18,  limit_max: 26)
-lab1.metric_statuses.create(metric: a_hum,   limit_min: 35,  limit_max: 70)
-lab1.metric_statuses.create(metric: u_cycle, limit_min: 600, limit_max: 900)
-lab1.metric_statuses.create(metric: l_cycle, limit_min: 540, limit_max: 840)
-lab1.metric_statuses.create(metric: no2,     limit_min: 0,   limit_max: 1)
-lab1.metric_statuses.create(metric: nh4,     limit_min: 0,   limit_max: 0.5)
-lab1.metric_statuses.create(metric: no3,     limit_min: 60,  limit_max: 130)
+[lab1, menucourt].each do |box|
+  lab1.metric_statuses.create(metric: ph,      limit_min: 6.5, limit_max: 7.8)
+  lab1.metric_statuses.create(metric: w_temp,  limit_min: 18,  limit_max: 25)
+  lab1.metric_statuses.create(metric: a_temp,  limit_min: 18,  limit_max: 26)
+  lab1.metric_statuses.create(metric: a_hum,   limit_min: 35,  limit_max: 70)
+  lab1.metric_statuses.create(metric: u_cycle, limit_min: 600, limit_max: 900)
+  lab1.metric_statuses.create(metric: l_cycle, limit_min: 540, limit_max: 840)
+  lab1.metric_statuses.create(metric: no2,     limit_min: 0,   limit_max: 1)
+  lab1.metric_statuses.create(metric: nh4,     limit_min: 0,   limit_max: 0.5)
+  lab1.metric_statuses.create(metric: no3,     limit_min: 60,  limit_max: 130)
+end
