5aae3f4caacc3128e69da04ba37880c7c9e9f721
nna <nico.nardone@gmail.com>
2015-03-19 23:14:48 +0100

    default time frame for graphs is 1 hour

--- app/controllers/demo/metrics_controller.rb
+++ app/controllers/demo/metrics_controller.rb
@@ -5,7 +5,7 @@ class Demo::MetricsController < ApplicationController
     @device = Risebox::Core::Device.find_by_key(params[:id])
     @metric = Risebox::Core::Metric.find_by_code params[:metric]
     @report_title = "Evolution de #{@metric.name}"
-    @begin_at = params[:begin_at].present? ? Date.parse(params[:begin_at]) : 1.day.ago
+    @begin_at = params[:begin_at].present? ? Date.parse(params[:begin_at]) : 1.hour.ago
     @result = format_result @device.measures.for_metric(@metric.code).select([:taken_at, :value]).since(@begin_at)
     @time_frames = time_frames
   end
