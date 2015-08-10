1271396d7ff79b128a5e4d0da8045e0c9cb2f0d7
nna <nico.nardone@gmail.com>
2015-08-10 19:34:34 +0200

    fix service loading in login controller

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,5 +1,6 @@
 class API::V1::LoginController < API::V1::AppSecuredController
   skip_before_filter :verify_authenticity_token
+  before_filter :load_service
 
   def login
     api_response @service.info
