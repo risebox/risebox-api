60a1b6545b0bf996c95aeeae9312ef5e24dd385e
nna <nico.nardone@gmail.com>
2015-08-11 17:06:28 +0200

    also return device token in info

--- app/services/risebox/query/registration.rb
+++ app/services/risebox/query/registration.rb
@@ -9,7 +9,7 @@ class Risebox::Query::Registration
     user   = registration.user
     device = registration.device
     [true, { user: {first_name: user.first_name, last_name: user.last_name, email: user.email} ,
-             device: {name: device.name, key: device.key} }]
+             device: {name: device.name, key: device.key, token: device.token} }]
   rescue
     [false, nil]
   end
