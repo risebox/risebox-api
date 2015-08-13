3b21a76eb7775b4a38493584d1f876734139e5f2
nna <nico.nardone@gmail.com>
2015-08-13 19:46:19 +0200

    send user.id to app after registration

--- app/services/risebox/query/registration.rb
+++ app/services/risebox/query/registration.rb
@@ -8,7 +8,7 @@ class Risebox::Query::Registration
   def info
     user   = registration.user
     device = registration.device
-    [true, { user: {first_name: user.first_name, last_name: user.last_name, email: user.email} ,
+    [true, { user: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email} ,
              device: {name: device.name, key: device.key, token: device.token} }]
   rescue
     [false, nil]
