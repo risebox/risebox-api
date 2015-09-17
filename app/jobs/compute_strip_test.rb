5fa43cf169abcb3477527cd32987c096216c50a1
nna <nico.nardone@gmail.com>
2015-09-17 23:18:15 +0200

    fix measure creation upon strip test

--- app/jobs/compute_strip_test.rb
+++ app/jobs/compute_strip_test.rb
@@ -9,12 +9,16 @@ class ComputeStripTest < JobBase
 
     # Create photo
     manager = Risebox::Manage::Strip.new(device)
-    strip_created, strip = manager.create(model, upload_key, DateTime.parse(tested_at))
+    strip_created, strip = manager.create(model, upload_key, tested_at)
 
     # Compute Photo to determine concentrations
     computer = Risebox::Compute::StripPhoto.new(device)
     strip_updated, strip = computer.compute strip.id
     puts "DONE computing result : #{strip_updated}"
+
+    # Store strip results as measures
+    measure_svc = Risebox::Query::Measure.new(device)
+    measure_svc.create_multiple_from_strip strip
 
     # Push notification
     notifier = Risebox::Notify::StripResult.new(device.owner)
