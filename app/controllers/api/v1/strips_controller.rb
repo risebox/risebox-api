0ca6b7a6aa539612598e3b61a2845c98c160db68
nna <nico.nardone@gmail.com>
2015-09-17 23:05:03 +0200

    strip test datetime is now an optional parameter

--- app/controllers/api/v1/strips_controller.rb
+++ app/controllers/api/v1/strips_controller.rb
@@ -2,17 +2,27 @@ class API::V1::StripsController < API::V1::DeviceSecuredController
   skip_before_filter :verify_authenticity_token
 
   def create
-    launched = JobRunner.run(ComputeStripTest, params[:model], @device.key, params[:upload_key], Time.now)
+    launched = JobRunner.run(ComputeStripTest, params[:model], @device.key, params[:upload_key], parse_date(params[:tested_at]))
     api_response [launched, nil]
   end
 
   def show
     svc = rescuer Risebox::Query::Strip.new(@device)
     res = svc.show(params[:id])
-    puts "Sending results back : #{res}"
     api_response res
   end
 
 private
+
+  def parse_date date
+    return nil if date.nil?
+    tested_at = nil
+    begin
+      tested_at = DateTime.parse(date)
+    rescue ArgumentError
+      puts "DateTime is invalid : will be nil"
+    end
+    tested_at
+  end
 
 end
