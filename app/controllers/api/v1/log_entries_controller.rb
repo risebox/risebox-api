565a6adfcc7ead5b5384f755842d0b0f78dfa131
nna <nico.nardone@gmail.com>
2015-09-28 17:21:09 +0200

    fix logs

--- app/controllers/api/v1/log_entries_controller.rb
+++ app/controllers/api/v1/log_entries_controller.rb
@@ -7,7 +7,7 @@ class API::V1::LogEntriesController < API::V1::DeviceSecuredController
   end
 
   def create
-    log_time = params[:logged_at].present? ? Time.at(params[:logged_at]).to_i).to_datetime : nil
+    log_time = params[:logged_at].present? ? Time.at(params[:logged_at].to_i).to_datetime : nil
     console.log("params[:logged_at] #{params[:logged_at]}")
     console.log("log_time #{log_time}")
     created, log_entry = @service.create(params[:level], params[:body], log_time)
