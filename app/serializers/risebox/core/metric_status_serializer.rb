7231fdcdc03c5ed0f768eb5a8e2674387c24a5bd
nna <nico.nardone@gmail.com>
2015-10-10 01:50:16 +0200

    improved metric status serializer

--- app/serializers/risebox/core/metric_status_serializer.rb
+++ app/serializers/risebox/core/metric_status_serializer.rb
@@ -1,10 +1,19 @@
 class Risebox::Core::MetricStatusSerializer < ActiveModel::Serializer
   root false
 
-  attributes :description, :level, :light, :limit_max, :limit_min, :metric_code, :value, :taken_at
+  attributes :description, :level, :light, :limit_max, :limit_min, :code, :value, :taken_at
 
-  def metric_code
+  def code
     object.metric.code
+  end
+
+  def value
+    if object.value.present?
+      object.metric.unit.present? ? "#{object.value} #{object.metric.unit}" : object.value.to_s
+    else
+      nil
+    end
+    "#{object.value} #{object.metric.unit}"
   end
 
 end
