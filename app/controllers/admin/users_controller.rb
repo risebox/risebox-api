f03664e10db3359f82703b387056a8e3768b0011
nna <nico.nardone@gmail.com>
2015-11-14 17:38:28 +0100

    Add and edit a user from BO

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,40 @@ class Admin::UsersController < Admin::BaseController
   def show
     @user = Risebox::Core::User.find(params[:id])
   end
+
+  def new
+    @user = Risebox::Core::User.new
+  end
+
+  def create
+    @user = Risebox::Core::User.new(user_params)
+    if @user.save
+      flash[:notice] = "User successfully created."
+      redirect_to admin_user_path(@user)
+    else
+      puts "@user.errors #{@user.errors.to_yaml}"
+      render :new
+    end
+  end
+
+  def edit
+    @user = Risebox::Core::User.find(params[:id])
+  end
+
+  def update
+    @user = Risebox::Core::User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = "User successfully updated."
+      redirect_to admin_user_path(@user)
+    else
+      puts "@user.errors #{@user.errors.to_yaml}"
+      render :edit
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:human, :first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
