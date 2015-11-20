054b6d202f5d7c7ff50ee8876a28d90c751da8c6
nna <nico.nardone@gmail.com>
2015-11-20 17:36:24 +0100

    app registers with email & password (not api token)

--- app/controllers/api/v1/user_secured_controller.rb
+++ app/controllers/api/v1/user_secured_controller.rb
@@ -2,8 +2,9 @@ class API::V1::UserSecuredController < API::V1::APIController
   before_action :secure_access
 private
   def secure_access
-  	user_email = request.headers['RISEBOX-USER-EMAIL']
-    user_api_token = request.headers['RISEBOX-USER-SECRET']
+  	user_email     = request.headers['RISEBOX-USER-EMAIL']
+    user_secret    = request.headers['RISEBOX-USER-SECRET']
+    user_api_token = request.headers['RISEBOX-API-SECRET']
     app_registration_token = request.headers['RISEBOX-APP-REGISTRATION-TOKEN']
 
     user_ok = false
@@ -14,8 +15,8 @@ private
         @app_registration = result
         @user = @app_registration.user
       end
-    elsif user_email.present? && user_api_token.present?
-      user_ok, result = rescuer(Risebox::Access::User.new).user_for_credentials(user_email, user_api_token)
+    elsif user_email.present? && (user_api_token.present? || user_secret.present?)
+      user_ok, result = rescuer(Risebox::Access::User.new).user_for_credentials(user_email, user_api_token || user_secret)
       if user_ok
         @user = result
       end
--- app/services/risebox/access/user.rb
+++ app/services/risebox/access/user.rb
@@ -1,10 +1,10 @@
 class Risebox::Access::User
 
-  def user_for_credentials email, api_token
+  def user_for_credentials email, token
 
     if email.present?
   	  user = Risebox::Core::User.where(email: email).first
-      user_logged = user.present? && Devise.secure_compare(user.api_token, api_token)
+      user_logged = user.present? && (Devise.secure_compare(user.api_token, token) || user.valid_password?(token))
     else
       user_logged = false
     end
