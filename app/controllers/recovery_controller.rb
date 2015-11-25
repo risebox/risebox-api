21f50b0b59da77e99f841934a2c8466464a7e12b
nna <nico.nardone@gmail.com>
2015-11-26 00:12:02 +0100

    sending direct email when receiving rollback api call

--- app/controllers/recovery_controller.rb
+++ app/controllers/recovery_controller.rb
@@ -4,7 +4,10 @@ class RecoveryController < ApplicationController
   def rollback
     puts "Recovery call received from device #{params[:device]}: recovering to version #{params[:version]}"
     if params[:device] && params[:version]
-      JobRunner.run(SendEmail, 'recovery_alert', params[:device], params[:version])
+      mail(to: "team@risebox.co",
+           body: "Risebox #{params[:device]} is rolling back to version #{params[:version]}. Check if everything is ok",
+           content_type: "text/html",
+           subject: "Risebox #{params[:device]} in recovery mode !")
     end
     render text: "Recovery acknowledged !"
   end
