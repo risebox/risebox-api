ffbcc5508082bdc2e8bbd9a8035bda582db005d6
Adrien Thery <adrien.thery@gmail.com>
2015-11-27 12:50:35 +0100

    protect rollback API and refactor

--- app/controllers/api/v1/brain_upgrade_controller.rb
+++ app/controllers/api/v1/brain_upgrade_controller.rb
@@ -4,13 +4,23 @@ class API::V1::BrainUpgradeController < API::V1::DeviceSecuredController
   before_action :load_service
 
   def upgrade
-    upgrade_time = params[:brain_upgraded_at].present? ? Time.at(params[:brain_upgraded_at].to_i).to_datetime : Time.now
-    puts "params[:version] #{params[:version]}"
-    upgraded, setting = @service.compute(params[:version].to_f, upgrade_time)
-    api_response [upgraded, setting]
+    puts "Brain acknowledging successful version upgrade to version #{params[:version]}"
+    api_response acknowledge_version_change
+  end
+
+  def rollback
+    puts "Recovery call received from device #{@device.name}: recovering to version #{params[:version]}"
+    UserMailer.recovery_alert(params[:device], params[:version]).deliver
+    api_response acknowledge_version_change
   end
 
 private
+
+  def acknowledge_version_change
+    upgrade_time = params[:brain_upgraded_at].present? ? Time.at(params[:brain_upgraded_at].to_i).to_datetime : Time.now
+    puts "Brain acknowledging change to version #{params[:version]}"
+    upgraded, setting = @service.compute(params[:version].to_f, upgrade_time)
+  end
 
   def load_service
     @service = rescuer Risebox::Compute::BrainUpgrade.new(@device.brain)
