60e113377bebd4175608aa60dc90f00affe11fa1
Nicolas NARDONE <nico.nardone@gmail.com>
2015-12-02 00:55:54 +0100

    app registration returns if user admin

--- app/services/risebox/query/app_registration.rb
+++ app/services/risebox/query/app_registration.rb
@@ -8,7 +8,7 @@ class Risebox::Query::AppRegistration
   def info
     user    = app_registration.user
     devices = user.devices
-    data = { user: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email, api_token: user.api_token} ,
+    data = { user: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email, api_token: user.api_token, admin: user.admin?} ,
              devices: [] }
     devices.each do |device|
       data[:devices] << {name: device.name, key: device.key}
