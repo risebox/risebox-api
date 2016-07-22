935efc08fd45152598b328bb2b11b3e85bf8b3f6
Nicolas NARDONE <nico.nardone@gmail.com>
2016-07-22 14:00:07 +0200

    measure are keeped for 1 week
    instead of 1 month

--- app/jobs/clean_old_data.rb
+++ app/jobs/clean_old_data.rb
@@ -6,10 +6,10 @@ class CleanOldData < JobBase
   def self.do_perform
     puts "START removing old data"
     #Remove Old Measures
-    Risebox::Core::Measure.where('taken_at < ?', 1.month.ago).destroy_all
+    Risebox::Core::Measure.where('taken_at < ?', 1.week.ago).delete_all
 
     #Remove Old Logs
-    Risebox::Core::LogEntry.where('created_at < ?', 1.day.ago).destroy_all
+    Risebox::Core::LogEntry.where('created_at < ?', 1.day.ago).delete_all
 
     puts "DONE removing old data"
   end
