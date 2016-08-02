4709bdd13d000dd863e55295c6112bd936dbfdde
Nicolas NARDONE <nico.nardone@gmail.com>
2016-08-02 08:31:49 +0200

    reduce measure retention to 6 days

--- app/jobs/clean_old_data.rb
+++ app/jobs/clean_old_data.rb
@@ -6,7 +6,7 @@ class CleanOldData < JobBase
   def self.do_perform
     puts "START removing old data"
     #Remove Old Measures
-    Risebox::Core::Measure.where('taken_at < ?', 1.week.ago).delete_all
+    Risebox::Core::Measure.where('taken_at < ?', 150.hours.ago).delete_all  #Between 6 and 7 days
 
     #Remove Old Logs
     Risebox::Core::LogEntry.where('created_at < ?', 1.day.ago).delete_all
